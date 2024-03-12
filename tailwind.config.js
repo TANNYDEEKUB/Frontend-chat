46730d529e2604acd33860ea058b2451d499b08e
Suphakit P <me@suphakit.net>
2024-03-12 19:27:54 +0700

    init<all>: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      whitesmoke: "#f5f5f5",
+      jet: "#2e2f2f",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
